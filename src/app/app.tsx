e3fb905e0232a8d48fac918367295b3ab83c913d
darker <darkerros@yandex.ru>
2023-05-24 02:44:26 +0300

    feat: add routing to app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Layout} from "../components/layout/layout";
+import {MainPage} from "../pages/main-page/main-page";
+import {UserInfoPage} from "../pages/user-info-page/user-info-page";
 
 function App() {
+
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Layout/>}>
+        <Route index element={<MainPage/>}/>
+        <Route path={'/user/:id'} element={<UserInfoPage/>}/>
+        <Route path={'/me'} element={<UserInfoPage/>}/>
+        <Route path={"*"} element={<MainPage/>}/>
+      </Route>
+    </Routes>
   );
 }
 
