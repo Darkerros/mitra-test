cb97b0adfb590d7e5e6bd8e7711740db784b1eda
darker <darkerros@yandex.ru>
2023-05-24 01:52:08 +0300

    feat: add user info reducer to root reducer

--- src/services/reducers/root-reducer.ts
+++ src/services/reducers/root-reducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {postsReducer} from "./posts-reducer";
 import {postCommentReducer} from "./post-comment-reducer";
+import {userInfoReducer} from "./user-info-reducer";
 
 export const rootReducer = combineReducers({
     postsReducer,
     postCommentReducer,
+    userInfoReducer
 })
