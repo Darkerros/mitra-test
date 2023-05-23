f4ec1fd2c8fa0a57beda109cf6bdf086e419e1d5
darker <darkerros@yandex.ru>
2023-05-24 01:11:41 +0300

    feat: create object from api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,33 +1,19 @@
-import axios, {AxiosInstance} from "axios";
+import axios from "axios";
 import {GetUserByIdResponse} from "./types/responses/get-user-by-id-response";
 import {GetPostCommentsResponse} from "./types/responses/get-post-comments-response";
 import {GetAllPostsResponse} from "./types/responses/get-all-posts-response";
 
-
-class Api {
-    private apiClient: AxiosInstance;
-
-    constructor() {
-        this.apiClient = axios.create({ baseURL: 'https://jsonplaceholder.typicode.com/' })
-    }
-
-    createReguest (endpoint: string) {
-        return this.apiClient.get(endpoint).then(resp => resp.data)
-    }
-
-    getAllPosts():GetAllPostsResponse {
-        return this.createReguest('/posts')
-    }
-
-    getPostComments(postId: number):GetPostCommentsResponse {
-        return this.createReguest(`/posts/${postId}/comments`)
-    }
-
-    getUserById(userId: number):GetUserByIdResponse {
-        return this.createReguest(`/users/${userId}`)
-    }
+const apiInstance = axios.create({ baseURL: 'https://jsonplaceholder.typicode.com/' })
+const createReguest =  (endpoint: string) => {
+    return apiInstance.get(endpoint).then(resp => resp.data)
 }
 
-const api = new Api()
+export const api = {
+    getAllPosts:():GetAllPostsResponse => createReguest('/posts'),
 
-export default api
+    getPostComments:(postId: number):GetPostCommentsResponse => createReguest(`/posts/${postId}/comments`),
+
+    getUserById:(userId: number):GetUserByIdResponse =>  createReguest(`/users/${userId}`)
+}
+
+
