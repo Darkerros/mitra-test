25ce4ce24b9b68d4744e0120fdcaeff7d12a17bd
darker <darkerros@yandex.ru>
2023-05-24 01:32:11 +0300

    feat: add post comment reducer to root reducer

--- src/services/reducers/root-reducer.ts
+++ src/services/reducers/root-reducer.ts
@@ -1,4 +1,8 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {postsReducer} from "./posts-reducer";
+import {postCommentReducer} from "./post-comment-reducer";
 
-export const rootReducer = combineReducers({postsReducer})
+export const rootReducer = combineReducers({
+    postsReducer,
+    postCommentReducer,
+})
