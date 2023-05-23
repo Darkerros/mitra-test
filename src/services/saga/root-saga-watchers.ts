410cfd5f81850a6097b99a40e602a0a1a9c56005
darker <darkerros@yandex.ru>
2023-05-24 01:38:45 +0300

    feat: create post comment watcher to root saga watcher

--- src/services/saga/root-saga-watchers.ts
+++ src/services/saga/root-saga-watchers.ts
@@ -1,6 +1,10 @@
 import {all} from "redux-saga/effects";
 import postsWatcher from "./posts-saga";
+import postCommentSaga from "./post-comment-saga";
 
 export function* rootSagaWatchers() {
-    yield all([postsWatcher()])
+    yield all([
+        postsWatcher(),
+        postCommentSaga(),
+    ])
 }
